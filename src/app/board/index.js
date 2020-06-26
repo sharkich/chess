2e9dab21a22d234d0c2bf1a6cb6a800f6d03f2b5
Artem Koziar <koziar.artem@gmail.com>
2020-06-26 14:18:31 +0300

    Selected V1

--- src/app/board/index.js
+++ src/app/board/index.js
@@ -1,6 +1,34 @@
 const ROW = { element: null, cells: [] };
 const CELL = { element: null };
 
+const onSelectPiece = (cell) => {
+  cell.piece.selected = !cell.piece.selected;
+  if (cell.piece.selected) {
+    cell.pieceElement.classList.add('selected');
+  } else {
+    cell.pieceElement.classList.remove('selected');
+  }
+};
+
+const onDropNewPiece = (cell, event) => {
+  cell.pieceElement.classList.remove('white-white');
+  cell.pieceElement.classList.remove('white-black');
+  cell.pieceElement.classList.remove('black-white');
+  cell.pieceElement.classList.remove('black-black');
+  const type = event.dataTransfer.getData('type');
+  const symbol = event.dataTransfer.getData('symbol');
+  const pieceColor = event.dataTransfer.getData('color');
+  cell.piece = {
+    type,
+    symbol,
+    pieceColor,
+    selected: false,
+  };
+  cell.pieceElement.innerText = symbol;
+  cell.pieceElement.classList.add(`${cell.color}-${pieceColor}`);
+  cell.pieceElement.onclick = () => onSelectPiece(cell);
+};
+
 const getCell = (root, rowIndex, cellIndex) => {
   const element = document.createElement('div');
   element.classList.add('cell');
@@ -12,22 +40,16 @@ const getCell = (root, rowIndex, cellIndex) => {
   element.appendChild(pieceElement);
 
   const cell = {
-    pieceElement,
+    color,
     element,
+    pieceElement,
+    piece: null,
   };
 
   element.ondragover = (event) => event.preventDefault();
   element.ondrop = (event) => {
     event.preventDefault();
-    pieceElement.classList.remove('white-white');
-    pieceElement.classList.remove('white-black');
-    pieceElement.classList.remove('black-white');
-    pieceElement.classList.remove('black-black');
-    const type = event.dataTransfer.getData('type');
-    const symbol = event.dataTransfer.getData('symbol');
-    const pieceColor = event.dataTransfer.getData('color');
-    pieceElement.innerText = symbol;
-    pieceElement.classList.add(`${color}-${pieceColor}`);
+    onDropNewPiece(cell, event);
   };
 
   root.appendChild(element);
