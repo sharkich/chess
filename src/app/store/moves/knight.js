6afd7d7e2f295f91464a2cbdc14178e01c9b7f9f
Artem Koziar <koziar.artem@gmail.com>
2020-06-29 16:29:05 +0300

    Implement Knight selected cells

--- src/app/store/moves/knight.js
+++ src/app/store/moves/knight.js
@@ -1,5 +1,9 @@
 const isCellsAvailableForKnightMove = (store, rowIndex, colIndex) => {
-  const selected = store.selected;
+  const { row, col, piece } = store.selected;
+  return (
+    (Math.abs(row - rowIndex) === 2 && Math.abs(col - colIndex) === 1) ||
+    (Math.abs(row - rowIndex) === 1 && Math.abs(col - colIndex) === 2)
+  );
 };
 
 export default isCellsAvailableForKnightMove;
