ca785d9201bc181b1e7466bb298eab04a1f37201
Artem Koziar <koziar.artem@gmail.com>
2020-06-30 19:31:17 +0300

    create render board function

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,11 @@
+import renderBoard from './render/board.js';
 console.log('app');
 
 const app = (root) => {
-  root.innerHTML = 'Hello, World!';
+  root.innerHTML = `
+    <div>Hello, World!</div>
+    ${renderBoard()}
+  `;
 };
 
 export default app;
