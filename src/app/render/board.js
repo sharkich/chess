1be104b1ccdf55ceb14153b96d1339c2de142cf9
Artem Koziar <koziar.artem@gmail.com>
2020-06-30 19:48:17 +0300

    render board

--- src/app/render/board.js
+++ src/app/render/board.js
@@ -1,5 +1,19 @@
-const renderBoard = () => `
-  <h1>Board</h1>
+const CELL_COUNT = 8;
+
+const renderBoard = () => {
+  const cells = new Array(CELL_COUNT)
+    .fill(null)
+    .map(() => `<div class="cell"></div>`)
+    .join('');
+  const rows = new Array(CELL_COUNT)
+    .fill(null)
+    .map(() => `<div class="row">${cells}</div>`)
+    .join('');
+  return `
+  <div class="board">
+    ${rows}
+  </div>
 `;
+};
 
 export default renderBoard;
