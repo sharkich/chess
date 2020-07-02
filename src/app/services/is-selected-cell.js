0cfd0c35f33751898a77d2fd475a64b9eac93723
Artem Koziar <koziar.artem@gmail.com>
2020-07-02 20:47:17 +0300

    Rook limits

--- src/app/services/is-selected-cell.js
+++ src/app/services/is-selected-cell.js
@@ -1,16 +1,32 @@
-const select = (bord, row, col) => {
-  if (bord[row][col]) {
-    bord[row][col].move = true;
-  }
-};
-
 const isSelectedCell = (row, col, board, selected) => {
   if (!selected.piece || board[row][col].piece) {
     return false;
   }
   switch (selected.piece.type) {
     case 'Rook':
-      return row === selected.row || col === selected.col;
+      if (row !== selected.row && col !== selected.col) {
+        return false;
+      }
+      if (row === selected.row) {
+        const min = Math.min(col, selected.col);
+        const max = Math.max(col, selected.col);
+        for (let i = min; i <= max; i++) {
+          if (i !== selected.col && board[row][i].piece) {
+            return false;
+          }
+        }
+        return true;
+      } else if (col === selected.col) {
+        const min = Math.min(row, selected.row);
+        const max = Math.max(row, selected.row);
+        for (let i = min; i <= max; i++) {
+          if (i !== selected.row && board[i][col].piece) {
+            return false;
+          }
+        }
+        return true;
+      }
+      return;
     case 'King':
     case 'Pawn':
     case 'Bishop':
